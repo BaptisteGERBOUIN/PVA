f29336a66a2ac7fe4afe8d88c1c1e96e40748674
Alexandre Leys <35851329+Myuketsu@users.noreply.github.com>
2023-11-24 01:41:19 +0100

    Update map

--- pages/alexandre.py
+++ pages/alexandre.py
@@ -1,24 +1,30 @@
 from dash import html, register_page, dcc
+import dash_bootstrap_components as dbc
 import plotly.express as px
 
 from data.api_fetcher import get_water_flow_data
 
 register_page(__name__, title='Water A')
 
+# --- HTML ---
+
 def layout():
     return html.Div(
-        [
-            dcc.Graph(figure=bar_charts()),
+        [   
+            dcc.RangeSlider(0, 2,
+                allowCross=False,
+                step=None,
+                marks={0: '11/03/2002', 1:'24/11/2023', 2:'31/12/2098'},
+                tooltip={'placement': 'bottom'}),
+            dbc.Tabs(
+                [
+                    dbc.Tab('cc', label='Qualité'),
+                    dbc.Tab('dd', label='Quantité')
+                ]
+            , style={'justifyContent': 'center'})
         ],
     )
 
-def bar_charts():
-    fig = px.bar(
-        get_water_flow_data(),
-        x='libelle_ecoulement',
-        y='Quantité',
-        color='libelle_ecoulement',
-        title='Quantité des différentes catégories d\'écoulement d\'eau en France sur le mois de Juin.',
-        log_y=True
-    )
-    return fig.update_layout(showlegend=False)
+# --- FIGURE ---
+
+# --- CALLBACKS ---
--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import pymongo
+import json
+import plotly.express as px
+import pandas as pd
+from random import randint
 
-client = pymongo.MongoClient('mongodb://localhost:27017/')
+# client = pymongo.MongoClient('mongodb://localhost:27017/')
 
-waterDB = client['water']
-nitrate = waterDB['nitrate']
+# waterDB = client['water']
+# nitrate = waterDB['nitrate']
 
 # nitrate.drop()
 # rand = [random.gauss() for _ in range(10_000)]
@@ -12,4 +16,19 @@ nitrate = waterDB['nitrate']
 # print(list(nitrate.aggregate([{'$group': {'_id': None, 'avgValue': {'$avg': '$value'}}}])))
 # print(statistics.mean(rand))
 
-print(list(nitrate.find()))
+# print(list(nitrate.find()))
+
+
+with open('./data/departements_france.geojson', 'r') as file:
+    js = json.load(file)
+    # print(len(js['features']))
+    # js['features'] = [i for i in js['features'] if i['properties']['code'] not in ['2A', '2B']]
+    # print(len(js['features']))
+    # file = open('./data/departements_france.geojson', 'w')
+    # json.dump(js, file)
+
+    df = pd.DataFrame({'code': [code['properties']['code'] for code in js['features']], 'value': [randint(0, 100) for _ in js['features']]})
+
+    fig = px.choropleth_mapbox(df, geojson=js, featureidkey='properties.code', color="value", locations='code')
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.show()
