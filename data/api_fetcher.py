769e69e8c1adf1f6f41a8cd9cd3816a5026b3b90
BaptisteGERBOUIN <102798518+BaptisteGERBOUIN@users.noreply.github.com>
2023-12-02 18:29:23 +0100

    Creation of parameters file and update api_fetcher

--- data/api_fetcher.py
+++ data/api_fetcher.py
@@ -1,47 +1,51 @@
 import requests as req
+from tqdm import tqdm
 import pandas as pd
 from pymongo import MongoClient
+from api_parameters import flowWaterObservation, flowWaterStation, qualityStation, qualityAnalyse, serviceIndicatorCommunes, serviceIndicatorIndicateurs, serviceIndicatorServices
 
+#----
+#Creation/Connexion à la DataBase
 
 database_name = "BDD"
 collection_name = "CollectionCoursdeau"
-
 client = MongoClient("mongodb://localhost:27017")
-
 db = client[database_name]
-
 collection = db[collection_name]
-
 print(f"La base de données '{database_name}' et la collection '{collection_name}' ont été créées avec succès.")
+#----
 
 
+#----
+#Fonction de recuperation des données
 
 def get_data(url: str, params: dict) -> dict:
     
     r = req.get(url=url, params=params)
     r.raise_for_status()
     return r.json().get('data', [])
+#----
 
+#----
+#Insertion des données dans la base de données
 
-def get_water_flow_data_obs() -> dict:
-    url = 'https://hubeau.eaufrance.fr/api/v1/ecoulement/observations'
-    params = {
-        'size': '2000',
-        'fields': 'libelle_ecoulement,code_station,libelle_station,code_departement,libelle_departement,code_commune,libelle_commune,code_region,libelle_region,coordonnee_x_station,coordonnee_y_station,code_bassin,libelle_bassin,code_cours_eau,libelle_cours_eau,date_observation,code_ecoulement,libelle_ecoulement,latitude,longitude',
-    }
-    
+def get_all_data_with_specified_parameters(api_params: dict) -> dict:
+
+    url = api_params['url']
+    params = api_params['parameters']
+
+    # Ajouter le champ 'fields' si présent dans les paramètres
+    fields = params.get('fields')
+    if fields:
+        params['fields'] = fields  
+
     all_data = [] #Liste pour stocker toutes les données
-    
-    
-    ceiling = 10000 # Nombre d'éléments à récupérer à chaque itération
-    
+    ceiling = 10_000 # Nombre d'éléments à récupérer à chaque itération
     start_index = 0
-
     total_iterations_limit = 1
     total_iterations = 0
     
     while total_iterations < total_iterations_limit:
-        
         params['start'] = start_index # on ajoute le paramètre 'start' pour définir l'index de départ
         data = get_data(url, params) # on appelle la fonction get_data pour récupérer les données depuis l'API
 
@@ -58,117 +62,36 @@ def get_water_flow_data_obs() -> dict:
         total_iterations += 1
     
     return all_data
+#----
 
+#----
 def insert_data_to_mongodb(data: list, database_name: str, collection_name: str):
-    
-    client = MongoClient("mongodb://localhost:27017") # Connexion à MongoDB
-    db = client[database_name]
-    collection = db[collection_name]
+    with MongoClient("mongodb://localhost:27017") as client: # Connexion à MongoDB
+        db = client[database_name]
+        collection = db[collection_name]
+        # collection.insert_many(data) # On insere les données dans la collection MongoDB
+        with tqdm(total=len(data), desc=f"Inserting data into {collection_name}", dynamic_ncols=True) as pbar:
+            for chunk in data:
+                collection.insert_one(chunk)
+                pbar.update(1)
+#----
 
-    
-    collection.insert_many(data) # On insere les données dans la collection MongoDB
-
-
+#----
 if __name__ == "__main__":
-    
-    data = get_water_flow_data_obs() # On recupere les données
-
     database_name = "BDD"
-    collection_name = "CollectionCoursdeau"
 
-    
-    insert_data_to_mongodb(data, database_name, collection_name) # On insere les données dans MongoDB
+    # Définir les paramètres pour chaque collection
+    collections = [
+        {'params': flowWaterObservation, 'name': 'flowWaterObservation'},
+        {'params': flowWaterStation, 'name': 'flowWaterStation'},
+        {'params': qualityStation, 'name': 'qualityStation'},
+        {'params': qualityAnalyse, 'name': 'qualityAnalyse'},
+        {'params': serviceIndicatorCommunes, 'name': 'serviceIndicatorCommunes'},
+        # {'params': serviceIndicatorIndicateurs, 'name': 'serviceIndicatorIndicateurs'},
+        # {'params': serviceIndicatorServices, 'name': 'serviceIndicatorServices'}
+    ]
 
+    for collection_info in collections:
+        data = get_all_data_with_specified_parameters(collection_info['params'])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Récupération des données de l'API cours d'eau
-
-# def get_water_flow_data_obs() -> dict: #Cours d'eau / observations 
-#     url = 'https://hubeau.eaufrance.fr/api/v1/ecoulement/observations'
-#     params = {
-#         'size': '20000',
-#         'date_observation_min': '2023-06-01',
-#         'date_observation_max': '2023-06-30',
-#         'fields': 'libelle_ecoulement,code_station,libelle_station,code_departement,libelle_departement,code_commune,libelle_commune,code_region,libelle_region,coordonnee_x_station,coordonnee_y_station,code_bassin,libelle_bassin,code_cours_eau,libelle_cours_eau,date_observation,code_ecoulement,libelle_ecoulement,latitude,longitude',
-#     }
-#     return get_data(url, params).fillna('Données manquantes').value_counts().reset_index(name='Quantité')
-
-def get_water_flow_data_station() -> dict: #Cours d'eau / station 
-    url = 'https://hubeau.eaufrance.fr/api/v1/ecoulement/stations'
-    params = {
-        'size': '20000',
-        'date_observation_min': '2023-06-01',
-        'date_observation_max': '2023-06-30',
-        'fields': 'code_station,libelle_station,code_departement,libelle_departement,code_commune,libelle_commune,code_region,libelle_region,coordonnee_x_station,coordonnee_y_station,code_cours_eau,libelle_cours_eau,date_maj_station,latitude,longitude',
-    }
-    return get_data(url, params).fillna('Données manquantes').value_counts().reset_index(name='Quantité')
-
-
-# datacoursdeau = get_water_flow_data_station()
-# datacoursdeau.to_csv('.datacoursdeau.csv')
-
-
-#Récupération des données de l'API Qualité de l'eau
-
-def get_water_quality_data_analyse() -> dict: #Qualité des cours d'eau / analyse_pc 
-    url = 'https://hubeau.eaufrance.fr/api/v2/qualite_rivieres/analyse_pc'
-    params = {
-        'size': '20000',
-        'date_observation_min': '2023-06-01',
-        'date_observation_max': '2023-06-30',
-        'fields': 'code_station, libelle_station,code_parametre,libelle_parametre,resultat,code_unite,symbole_unite,code_remarque,date_maj_analyse,longitude,latitude',
-    }
-    return get_data(url, params).fillna('Données manquantes').value_counts().reset_index(name='Quantité')
-
-
-def get_water_quality_data_station() -> dict: #Qualité des cours d'eau / station_pc 
-    url = 'https://hubeau.eaufrance.fr/api/v2/qualite_rivieres/station_pc'
-    params = {
-        'size': '20000',
-        'date_observation_min': '2023-06-01',
-        'date_observation_max': '2023-06-30',
-        'fields': 'code_station,libelle_station,coordonnee_x,coordonnee_y,longitude,latitude,code_commune,libelle_commune,code_departement,libelle_departement,code_region,libelle_region,code_cours_eau,date_maj_information',
-    }
-    return get_data(url, params).fillna('Données manquantes').value_counts().reset_index(name='Quantité')
-
-
-
-#Récupération des données de l'API Indicateurs des services
-
-
-def get_water_quality_data_analyse() -> dict: #Qualité des cours d'eau / analyse_pc 
-    url = 'https://hubeau.eaufrance.fr/api/v2/qualite_rivieres/analyse_pc'
-    params = {
-        'size': '20000',
-        'date_observation_min': '2023-06-01',
-        'date_observation_max': '2023-06-30',
-        'fields': 'libelle_ecoulement,code_station,libelle_station,code_departement,libelle_departement,code_commune,libelle_commune,code_region,libelle_region,coordonnee_x_station,coordonnee_y_station,code_bassin,libelle_bassin,code_cours_eau,libelle_cours_eau,date_observation,code_ecoulement,libelle_ecoulement,latitude,longitude',
-    }
-    return get_data(url, params).fillna('Données manquantes').value_counts().reset_index(name='Quantité')
-
-
-def get_water_quality_data_station() -> dict: #Qualité des cours d'eau / station_pc 
-    url = 'https://hubeau.eaufrance.fr/api/v2/qualite_rivieres/station_pc'
-    params = {
-        'size': '20000',
-        'date_observation_min': '2023-06-01',
-        'date_observation_max': '2023-06-30',
-        'fields': 'libelle_ecoulement,code_station,libelle_station,code_departement,libelle_departement,code_commune,libelle_commune,code_region,libelle_region,coordonnee_x_station,coordonnee_y_station,code_bassin,libelle_bassin,code_cours_eau,libelle_cours_eau,date_observation,code_ecoulement,libelle_ecoulement,latitude,longitude',
-    }
-    return get_data(url, params).fillna('Données manquantes').value_counts().reset_index(name='Quantité')
-
-# data2 = get_data('https://hubeau.eaufrance.fr/api/v0/indicateurs_services/indicateurs')
-# data2.to_csv('.toto.csv')
+        insert_data_to_mongodb(data, database_name, collection_info['name'])
