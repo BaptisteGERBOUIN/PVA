d7cad9ecbcf91dbb1f9b1458c752ab9acab7aef1
Alexandre Leys <35851329+Myuketsu@users.noreply.github.com>
2023-12-04 22:09:28 +0100

    map update - zoomable

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PVA
+
+## Installation
+
+La commande suivante permet d'installer toutes les bibliothèques nécessaires au projet.  
+Attention à bien exécuter la commande dans le dossier qui contient **"requirements.txt"**.  
+
+-  ```pip install -r requirements.txt```
+
 source /net/ens/python-dash/bin/activate
--- pages/alexandre.py
+++ pages/alexandre.py
@@ -1,14 +1,13 @@
 from datetime import date
 
-from dash import register_page, Input, Output, State, no_update, callback
+from dash import register_page, Input, Output, State, no_update, callback, ctx
 from dash import html, dcc
 
-from data.api_fetcher import get_water_flow_data
-from data.geojson_processing import FRANCE, GeographicArea
+from data.geojson_processing import FRANCE
 
 import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
 import dash_leaflet as dl
-import plotly.express as px
 
 register_page(__name__, title='Water A')
 
@@ -16,26 +15,16 @@ register_page(__name__, title='Water A')
 
 def layout():
     return [
-        dcc.Store(id='path-to-area', data=['France']),
+        dcc.Store(id='path_to_area', data=['France']),
         html.Div(
             [
                 menu_map(),
-                dl.Map(
+                html.Div(
                     [
-                        dl.TileLayer(),
-                        dl.GeoJSON(
-                            data=FRANCE.gdf_to_json(),
-                            zoomToBounds=True,
-                            zoomToBoundsOnClick=True,
-                            hoverStyle={'color': '#666'},
-                            id='geojson_mapbox'
-                        ),
-                        dl.EasyButton(icon="bi bi-house-door", title="Home France", id="btn_home_france")
+                        breadcrumbs_map(),
+                        viewport_map()
                     ],
-                    zoomSnap=0.2,
-                    dragging=True,
-                    maxBounds=FRANCE.getMaxBounds(),
-                    id='zoomable_map'
+                    id='map_box'
                 )
             ],
             id='map_container'
@@ -64,59 +53,121 @@ def menu_map():
         id='map_menu_container'
     )
 
-# --- FIGURE ---
-
-def get_choropleth(geoArea: GeographicArea):
-    fig = px.choropleth(
-        data_frame=geoArea.gdf, 
-        geojson=geoArea.gdf, 
-        locations=geoArea.gdf.index, 
-        scope='europe',
-        fitbounds='locations',
-        hover_name='name',
-        basemap_visible=False
+def breadcrumbs_map():
+    return dmc.Breadcrumbs(
+        id='path_display',
+        separator='→',
+        children=[
+            html.Div('France', id='path_root', className='path_display'),
+            html.Div(' - ', id='path_region', className='path_display'),
+            html.Div(' - ', id='path_departement', className='path_display')
+        ]
     )
 
-    fig.update_layout(
-        showlegend=False, 
-        paper_bgcolor='rgba(0,0,0,0)',
-        margin={'r': 0, 't': 0, 'l': 0, 'b': 0}
+def viewport_map():
+    return dl.Map(
+        [
+            # dl.TileLayer(), #add basemap (open-street-map)
+            dl.GeoJSON(
+                data=FRANCE.gdf_to_json(),
+                zoomToBounds=True,
+                hoverStyle={'color': '#666'},
+                id='geojson_mapbox'
+            ),
+            dl.EasyButton(n_clicks=0, icon='bi bi-house-door', title='Voir France entière', id='btn_home_france')
+        ],
+        zoomSnap=0.2,
+        zoomControl=False,
+        scrollWheelZoom=False,
+        dragging=False,
+        doubleClickZoom=False,
+        id='zoomable_map'
     )
-
-    return fig
 
 # --- CALLBACKS ---
 
 @callback(
-    [Output('map', 'figure'),
-     Output('path-to-area', 'data'),
-     Output('map', 'clickData')],
-    [Input('map', 'clickData'),
-     Input('box-map', 'n_clicks')],
-    [State('path-to-area', 'data'),
-     State('map', 'hoverData')],
+    [
+        Output('geojson_mapbox', 'data'),
+        Output('geojson_mapbox', 'clickData'),
+        Output('path_to_area', 'data'),
+
+        Output('path_root', 'children'),
+        Output('path_region', 'children'),
+        Output('path_departement', 'children'),
+    ],
+    [
+        Input('geojson_mapbox', 'clickData'),
+        Input('btn_home_france', 'n_clicks'),
+
+        Input('path_root', 'n_clicks'),
+        Input('path_region', 'n_clicks'),
+        Input('path_departement', 'n_clicks'),
+    ],
+    [
+        State('path_to_area', 'data'),
+    ],
     prevent_initial_call=True)
-def update_map_on_click(clickData, nClicks, pathToArea, hoverMap):
-    if hoverMap is None:
-        return *click_outside_area(pathToArea), None
-    
-    if clickData is None:
-        return no_update, no_update, None
+def update_map_and_path_on_click(inClickData, inBtn, inPathRoot, inPathRegion, inPathDepart, statePathToArea):
+    triggeredId = ctx.triggered_id
 
-    return *click_inside_area(clickData, pathToArea), None
-    
-def click_inside_area(clickData, pathToArea):
-    area_name = clickData['points'][0].get('hovertext')
-    if area_name is None:
+    geosjonClickData = None
+    geosjonData, pathData = [no_update] * 2
+    pathRoot, pathRegion, pathDepartement = [no_update] * 3
+
+    if triggeredId == 'btn_home_france':
+        geosjonData, pathData = btn_home_map(inBtn)
+
+    if triggeredId == 'geojson_mapbox':
+        geosjonData, pathData = go_in_area(inClickData, statePathToArea)
+
+    if triggeredId in ['path_root', 'path_region', 'path_departement']:
+        geosjonData, pathData = click_on_path_display(triggeredId, statePathToArea)
+
+    pathRoot, pathRegion, pathDepartement = update_path_display(pathData)
+
+    return geosjonData, geosjonClickData, pathData, pathRoot, pathRegion, pathDepartement
+
+def btn_home_map(btn: int):
+    """
+    Manages the button that takes you back to the most zoomed-out area (France).
+    """
+    if not btn:
         return no_update, no_update
+    return FRANCE.gdf_to_json(), ['France']
 
-    if pathToArea[-1] != area_name:
-        pathToArea = pathToArea + [area_name]
-        return get_choropleth(FRANCE.get_from_path(pathToArea)), pathToArea
-    return get_choropleth(FRANCE.get_from_path(pathToArea)), no_update
+def go_in_area(clickData: dict, pathToArea: list):
+    """
+    Manages a click event to navigate to a specific zone based on the click data 
+    supplied and the current path to the zone.
+    """
+    if clickData is None:
+        return no_update, no_update
+    areaName = clickData['properties']['name']
 
-def click_outside_area(pathToArea):
-    if len(pathToArea) > 1:
-        pathToArea = pathToArea[:-1]
-        return get_choropleth(FRANCE.get_from_path(pathToArea)), pathToArea
-    return get_choropleth(FRANCE), no_update
+    if pathToArea[-1] != areaName:
+        pathToArea = pathToArea + [areaName]
+        return FRANCE.get_from_path(pathToArea).gdf_to_json(), pathToArea
+    return FRANCE.get_from_path(pathToArea).gdf_to_json(), no_update
+
+def click_on_path_display(triggeredId: str, pathToArea: list):
+    '''
+    Display the path by updating the map of the selected zone based on the trigger 
+    identifier supplied and the current path to the zone.
+    '''
+    for indexPath, pathName in enumerate(['path_root', 'path_region', 'path_departement']):
+        if triggeredId != pathName:
+            continue
+        if len(pathToArea) == indexPath + 1:
+            return [no_update] * 2
+        pathToArea = pathToArea[:indexPath + 1]
+        break
+    return FRANCE.get_from_path(pathToArea).gdf_to_json(), pathToArea
+
+def update_path_display(pathData: list):
+    """
+    Update the path display information with a given path data.
+    """
+    if pathData == no_update:
+        return [no_update] * 3
+    return *pathData, *[' - ' for _ in range(3 - len(pathData))]
