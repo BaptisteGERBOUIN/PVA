7ed207e10af9c9b1e31eeaff53fd1c9864f68931
Alexandre Leys <35851329+Myuketsu@users.noreply.github.com>
2023-12-06 00:37:35 +0100

    update map & menu

--- pages/alexandre.py
+++ pages/alexandre.py
@@ -21,7 +21,6 @@ def layout():
                 menu_map(),
                 html.Div(
                     [
-                        breadcrumbs_map(),
                         viewport_map()
                     ],
                     id='map_box'
@@ -34,14 +33,6 @@ def layout():
 def menu_map():
     return html.Div(
         [
-            dbc.DropdownMenu(
-                label='Type de la carte',
-                children=[
-                    dbc.DropdownMenuItem('Qualité'),
-                    dbc.DropdownMenuItem('Quantité'),
-                ],
-                size='lg'
-            ),
             dcc.DatePickerRange(
                 id='my-date-picker-range',
                 min_date_allowed=date(1995, 8, 5),
@@ -51,17 +42,6 @@ def menu_map():
             ),
         ],
         id='map_menu_container'
-    )
-
-def breadcrumbs_map():
-    return dmc.Breadcrumbs(
-        id='path_display',
-        separator='→',
-        children=[
-            html.Div('France', id='path_root', className='path_display'),
-            html.Div(' - ', id='path_region', className='path_display'),
-            html.Div(' - ', id='path_departement', className='path_display')
-        ]
     )
 
 def viewport_map():
@@ -74,12 +54,15 @@ def viewport_map():
                 hoverStyle={'color': '#666'},
                 id='geojson_mapbox'
             ),
-            dl.EasyButton(n_clicks=0, icon='bi bi-house-door', title='Voir France entière', id='btn_home_france')
+            dl.EasyButton(n_clicks=0, icon='bi bi-house-door', title='Voir France entière', id='btn_home_france'),
+            dl.EasyButton(n_clicks=0, icon='bi bi-arrow-90deg-left', title='Revenir en arrière', id='btn_backward'),
+            dmc.SegmentedControl(data=['Qualité', 'Quantité'], radius='md', id='seg_control')
         ],
         zoomSnap=0.2,
         zoomControl=False,
         scrollWheelZoom=False,
         dragging=False,
+        attributionControl=False,
         doubleClickZoom=False,
         id='zoomable_map'
     )
@@ -89,44 +72,35 @@ def viewport_map():
 @callback(
     [
         Output('geojson_mapbox', 'data'),
-        Output('geojson_mapbox', 'clickData'),
         Output('path_to_area', 'data'),
 
-        Output('path_root', 'children'),
-        Output('path_region', 'children'),
-        Output('path_departement', 'children'),
+        Output('geojson_mapbox', 'clickData'),
     ],
     [
         Input('geojson_mapbox', 'clickData'),
         Input('btn_home_france', 'n_clicks'),
-
-        Input('path_root', 'n_clicks'),
-        Input('path_region', 'n_clicks'),
-        Input('path_departement', 'n_clicks'),
+        Input('btn_backward', 'n_clicks'),
     ],
     [
         State('path_to_area', 'data'),
     ],
     prevent_initial_call=True)
-def update_map_and_path_on_click(inClickData, inBtn, inPathRoot, inPathRegion, inPathDepart, statePathToArea):
+def update_map_and_path_on_click(inClickData, inBtnHome, inBtnBack, statePathToArea):
     triggeredId = ctx.triggered_id
 
-    geosjonClickData = None
     geosjonData, pathData = [no_update] * 2
-    pathRoot, pathRegion, pathDepartement = [no_update] * 3
+    geosjonClickData = None
 
     if triggeredId == 'btn_home_france':
-        geosjonData, pathData = btn_home_map(inBtn)
+        geosjonData, pathData = btn_home_map(inBtnHome)
+    
+    if triggeredId == 'btn_backward':
+        geosjonData, pathData = btn_backward(inBtnBack, statePathToArea)
 
     if triggeredId == 'geojson_mapbox':
         geosjonData, pathData = go_in_area(inClickData, statePathToArea)
 
-    if triggeredId in ['path_root', 'path_region', 'path_departement']:
-        geosjonData, pathData = click_on_path_display(triggeredId, statePathToArea)
-
-    pathRoot, pathRegion, pathDepartement = update_path_display(pathData)
-
-    return geosjonData, geosjonClickData, pathData, pathRoot, pathRegion, pathDepartement
+    return geosjonData, pathData, geosjonClickData
 
 def btn_home_map(btn: int):
     """
@@ -134,6 +108,13 @@ def btn_home_map(btn: int):
     """
     if not btn:
         return no_update, no_update
+    return FRANCE.gdf_to_json(), ['France']
+
+def btn_backward(btn: int, pathToArea: list):
+    if not btn:
+        return no_update, no_update
+    if len(pathToArea[:-1]) > 1:
+        return FRANCE.get_from_path(pathToArea[:-1]).gdf_to_json(), pathToArea[:-1]
     return FRANCE.gdf_to_json(), ['France']
 
 def go_in_area(clickData: dict, pathToArea: list):
@@ -149,25 +130,3 @@ def go_in_area(clickData: dict, pathToArea: list):
         pathToArea = pathToArea + [areaName]
         return FRANCE.get_from_path(pathToArea).gdf_to_json(), pathToArea
     return FRANCE.get_from_path(pathToArea).gdf_to_json(), no_update
-
-def click_on_path_display(triggeredId: str, pathToArea: list):
-    '''
-    Display the path by updating the map of the selected zone based on the trigger 
-    identifier supplied and the current path to the zone.
-    '''
-    for indexPath, pathName in enumerate(['path_root', 'path_region', 'path_departement']):
-        if triggeredId != pathName:
-            continue
-        if len(pathToArea) == indexPath + 1:
-            return [no_update] * 2
-        pathToArea = pathToArea[:indexPath + 1]
-        break
-    return FRANCE.get_from_path(pathToArea).gdf_to_json(), pathToArea
-
-def update_path_display(pathData: list):
-    """
-    Update the path display information with a given path data.
-    """
-    if pathData == no_update:
-        return [no_update] * 3
-    return *pathData, *[' - ' for _ in range(3 - len(pathData))]
