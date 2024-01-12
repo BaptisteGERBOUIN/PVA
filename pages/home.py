f61a600641133fe4969b6a1dbf7b2236263aa9b1
Mine <myuketsu@gmail.com>
2024-01-12 06:48:08 +0100

    register_page

--- pages/home.py
+++ pages/home.py
@@ -1,6 +1,7 @@
 from dash import html, register_page
 
-register_page(__name__, path='/', title='PVA')
+register_page(__name__, path='/', name='Menu', title='PVA', order=1,
+              category='Accueil', icon='bi bi-house')
 
 def layout():
     return html.Div(
